df83f4cd6d0500b3bd4bcbb21265efea33997f17
AsHansen88 <100618999+AsHansen88@users.noreply.github.com>
2022-03-02 21:09:40 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,72 +1,98 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public void Bogstaver_til_tal() {
+    Scanner scan = new Scanner(System.in);
 
-        String s = "abcdefghijklmnopqrstuvwxyz";
-        String t = "";
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (!t.isEmpty()) {
-                t += "";
-            }
-            int n = (int)ch - (int)'a' + 1;
-            t += String.valueOf(n);
-        }
-
-        }
-
-    public void Tal_til_bogstaver() {
-
-        char[] alfabet = new char[26];
-        alfabet[1] = 'A';
-        alfabet[2] = 'B';
-        alfabet[3] = 'C';
-        alfabet[4] = 'D';
-        alfabet[5] = 'E';
-        alfabet[6] = 'F';
-        alfabet[7] = 'G';
-        alfabet[8] = 'H';
-        alfabet[9] = 'I';
-        alfabet[10] = 'J';
-        alfabet[11] = 'K';
-        alfabet[12] = 'L';
-        alfabet[13] = 'M';
-        alfabet[14] = 'N';
-        alfabet[15] = 'O';
-        alfabet[16] = 'P';
-        alfabet[17] = 'Q';
-        alfabet[18] = 'R';
-        alfabet[19] = 'S';
-        alfabet[20] = 'T';
-        alfabet[21] = 'U';
-        alfabet[22] = 'V';
-        alfabet[23] = 'W';
-        alfabet[24] = 'X';
-        alfabet[25] = 'Y';
-        alfabet[25] = 'Z';
-
-        for(int i = 0; i < alfabet.length; i++)
-
-
-    }
+    //Variabler
+    int Bruger;
+    int Shift;
+    String alfabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    String Besked;
+    String ciphertekst = "";
 
     public static void main(String[] args) {
 
-        new Main().Tal_til_bogstaver();
-        {
+        new Main().Velkommen();
+        new Main().Cæsar();
 
-        }
-        new Main().Bogstaver_til_tal();
-        {
+    }
 
+    public void Velkommen() {
+
+        System.out.println("Velkommen til krypterings- og dekrypteringsmaskinen");
+        System.out.println("Du har nu 4 valg muligheder: ");
+        System.out.println("Tast 1 for Cæsar Cipher: " + Bruger);
+        System.out.println("Tast 2 for exit: " + Bruger);
+
+    }
+
+    public void Cæsar() {
+
+        Bruger = scan.nextInt();
+        if (Bruger == 1) {
+            System.out.println("Du har valgt Cæsar Cipher: ");
+            System.out.println("Du har nu 2 valgmuligheder: ");
+            System.out.println("Tast 1 for Kryptering");
+            System.out.println("Tast 2 for Dekryptering");
+            Bruger = scan.nextInt();
+
+            if (Bruger == 1) {
+
+                System.out.println("Du har valgt Kryptering");
+
+                System.out.println("skriv en besked for kryptering: ");
+                Besked = scan.next();
+
+                System.out.println("Skriv en shift værdi");
+                Shift = scan.nextInt();
+
+                Besked = Besked.toUpperCase();
+                for (int i = 0; i < Besked.length(); i++) {
+                    int charPosition = alfabet.indexOf(Besked.charAt(i));
+                    int værdi = (Shift + charPosition) % 26;
+                    char replaceVal = alfabet.charAt(værdi);
+                    ciphertekst += replaceVal;
+                }
+
+                System.out.println("Den krypterede besked er:" + ciphertekst);
+            }
+
+        }if (Bruger == 2) {
+            System.out.println("Du har valgt Dekryptering");
+
+            System.out.println("skriv den dekrypterede besked: ");
+            Besked = scan.next();
+
+            System.out.println("Skriv en shift værdi");
+            Shift = scan.nextInt();
+
+            Besked = Besked.toUpperCase();
+            for (int i = 0; i < Besked.length(); i++) {
+                int charPosition = alfabet.indexOf(Besked.charAt(i));
+                int værdi = (charPosition - Shift) % 26;
+                if (værdi < 0) {
+                    værdi = alfabet.length() + værdi;
+                }
+                char replaceVal = alfabet.charAt(værdi);
+                ciphertekst += replaceVal;
+
+            }
+            System.out.println("Den Dekrypterede besked er: " + ciphertekst);
         }
+    }
 
     }
 
 
-    }
+
+
+
+
+
+
 
 
 
